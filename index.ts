481ded8e26f0358528b0c40ba759c2423c5c7165
unknown <rafa-asat@yandex.ru>
2024-12-17 01:37:29 +0300

    nt

---
+++ index.ts
@@ -0,0 +1,18 @@
+// import {Address, TonClient} from "ton"
+// import {getHttpEndpoint} from "@orbs-network/ton-access";
+
+// async function main() {
+//     const wallet = Address.parse('0QDfLQYKgYorseMZM7sZFuNQgCVFVylf1LII0c59iPI0iiud');
+//     const collection = Address.parse('EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX');    
+
+//     const endpoint = await getHttpEndpoint({
+//         network: "testnet",
+//     });
+
+//     // ... previous code
+//     const miningData = await client.callGetMethod(collection, 'get_mining_data')
+
+//     console.log(miningData)
+// }
+
+// main()
